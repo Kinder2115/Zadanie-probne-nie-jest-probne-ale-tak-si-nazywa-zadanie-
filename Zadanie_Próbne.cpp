d4d31cdeae2e1cd3d39a90bc1a7a38cf7649064e
Kinder2115 <160577205+Kinder2115@users.noreply.github.com>
2024-02-25 22:49:46 +0100

    Create Zadanie_Próbne.cpp

---
+++ Zadanie_Próbne.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    int A;
+    int B;
+    int suma;
+    cin >> A;
+    cin >> B;
+    
+    if(A< 200 and B < 200) {
+        suma = A + B;
+        cout << suma;
+    }
+    return 0;
+}
